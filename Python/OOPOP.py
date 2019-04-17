63dfcee6e6fbec15ffdda21c15415bf0123746c8
ktongs <kingsley@eneris.ca>
2019-04-17 16:44:32 -0700

    Added python rfolder to repo

---
+++ Python/OOPOP.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def speak(self,voice):
+        print(voice + "!")
+
+class Cat(Animal):
+    def __init(self,numLegs):
+        self.name
+
+dog = Animal('Bobby',10)
+
+print(dog.name)
+print(dog.age)
+dog.speak('bark')
+
+dog.age = 50
+
+print(dog.age)
