258457aae3a12e54ac6575d5120331234c93a783
ktongs <kingsleytong@hotmail.com>
2019-04-18 14:16:16 -0700

    added Python libraries for queueing and stacking

---
+++ Python/simple_queue_and_stack.py
@@ -0,0 +1,20 @@
+from queue import *
+# class queue():
+#     def __init__(self,):
+
+arr = [1,2,3]
+
+arr.insert(1,4)
+# arr.append(2)
+# arr.pop()
+arr[1] = 5
+print(arr.pop())
+print(arr)
+print(type(arr))
+
+q = Queue(0)
+q.put(1)
+q.put(2)
+
+print(q.get())
+print(q.get())
